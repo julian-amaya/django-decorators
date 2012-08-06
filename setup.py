60ce072f44f5fe98d66940fd676651176b0776c2
Julian Amaya <julian@monoku.com>
2012-08-06 18:33:47 -0500

    ready for pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-decorators',
+    version='0.1.0',
+    author='Julian Amaya',
+    author_email='julian@monoku.com',
+    packages=['django_decorators'],
+    url='http://pypi.python.org/pypi/django-decorators/',
+    license='LICENSE.md',
+    description='A bunch of django extra decorators.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "Django >= 1.1.1",
+    ],
+)
