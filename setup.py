6a1431fc4e9acb7b466a9bd9a405dc33bfc09320
Julian Amaya <julian@monoku.com>
2013-01-14 10:34:32 -0500

    new version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-decorators',
-    version='0.1.0',
+    version='0.1.1',
     author='Julian Amaya',
     author_email='julian@monoku.com',
     packages=['django_decorators'],
